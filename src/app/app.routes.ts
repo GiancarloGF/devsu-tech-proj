cca3c82a99a5e063225fac0ad8e0c7fb0e47fc78
Giancarlo Guerra <gian.guerra23@gmail.com>
2026-06-03 12:39:51 -0500

    chore: configurar rutas principales de app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('@features/products/presentation/products.routes').then(m => m.routes),
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+  },
+];
